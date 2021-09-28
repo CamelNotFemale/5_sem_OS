52bf32d5dadb66948ebdab694ad9fb408e76db9f
boris_nem <58486153+CamelNotFemale@users.noreply.github.com>
2021-09-29 02:15:09 +0300

    changing fileSize to LARGE_INTEGER, ended part2

--- lab1/part2.cpp
+++ lab1/part2.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <cmath>
-#include <Windows.h>
+#define _WIN32_WINNT 0x0501
+#include <windows.h>
 using namespace std;
 // глобальная переменная, хранящая количество вызовов окончания операций ввода-вывода
 int callback = 0;
 // хэндлы на файлы, учавствующие в операции копирования
 HANDLE firstHandle;
 HANDLE secondHandle;
+// текущий сдвиг состояния копирования файла для операций чтения и записи соответственно
+LARGE_INTEGER shiftRead;
+LARGE_INTEGER shiftWrite;
 
 // Функция завершения, которая будет вызываться всякий раз при завершении операции ввода-вывода
 VOID CALLBACK FileIOCompletionRoutine(DWORD dwErrorCode, DWORD dwNumberOfBytesTransfered, LPOVERLAPPED lpOverlapped) {
     // количество вызовов
     callback++;
 }
-void ReadFileOverlapped(DWORD fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle);
-void WriteFileOverlapped(DWORD fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle);
+void ReadFileOverlapped(long long fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle);
+void WriteFileOverlapped(long long fileSize, DWORD blockSize, int operationsCount, OVERLAPPED* overlappeds, CHAR** buffer, HANDLE fileHandle);
 // Функция асинхронного копирования
 void copyFile(HANDLE firstHandle, HANDLE secondHandle, DWORD blockSize, int operationsCount);
 
@@ -92,7 +96,9 @@ void ReadFileOverlapped(long long fileSize, DWORD blockSize, int operationsCount
         SleepEx(-1, true);
     for (int i=0; i<operationsCount; i++)
     {
-        overlappeds[i].Offset += blockSize*operationsCount;
+        overlappeds[i].Offset = shiftRead.LowPart;
+        overlappeds[i].OffsetHigh = shiftRead.HighPart;
+        shiftRead.QuadPart += blockSize;
     }
     callback = 0;
 }
@@ -113,36 +119,48 @@ void WriteFileOverlapped(long long fileSize, DWORD blockSize, int operationsCoun
         SleepEx(-1, true);
     for (int i=0; i<operationsCount; i++)
     {
-        overlappeds[i].Offset += blockSize*operationsCount;
+        overlappeds[i].Offset = shiftWrite.LowPart;
+        overlappeds[i].OffsetHigh = shiftWrite.HighPart;
+        shiftWrite.QuadPart += blockSize;
     }
     callback = 0;
 }
 
 void copyFile(HANDLE firstHandle, HANDLE secondHandle, DWORD blockSize, int operationsCount) {
     DWORD high = 0;
-    long long fileSize = GetFileSize(firstHandle, &high);
+    LARGE_INTEGER fileSizeStruct;
+    long long fileSize;
+    GetFileSizeEx(firstHandle, &fileSizeStruct);
+    fileSize = fileSizeStruct.QuadPart;
     long long curSize = fileSize;
+
     CHAR** buffer = new CHAR*[operationsCount];
+    for (int i=0; i < operationsCount; i++)
+    {
+        buffer[i] = new CHAR[(int)blockSize];
+    }
     // должны оставаться допустимыми для длительной операции чтения.
     OVERLAPPED* over_1 = new OVERLAPPED[operationsCount];
     OVERLAPPED* over_2 = new OVERLAPPED[operationsCount];
 
+    shiftRead.QuadPart = 0;
+    shiftWrite.QuadPart = 0;
     for (int i=0; i < operationsCount; i++)
     {
-        buffer[i] = new CHAR[(int)blockSize];
-
-        over_1[i].Offset = over_2[i].Offset = i*(int)blockSize;
-        over_1[i].OffsetHigh = over_2[i].OffsetHigh = i*(int)high;
+        over_1[i].Offset = over_2[i].Offset = shiftRead.LowPart;
+        over_1[i].OffsetHigh = over_2[i].OffsetHigh = shiftRead.HighPart;
         over_1[i].hEvent = over_2[i].hEvent = NULL;
+        shiftRead.QuadPart += blockSize;
+        shiftWrite.QuadPart += blockSize;
     }
 
     do
     {
-        ReadFileOverlapped(fileSize, blockSize, operationsCount, over_1, buffer, firstHandle);
-        WriteFileOverlapped(fileSize, blockSize, operationsCount, over_2, buffer, secondHandle);
+        ReadFileOverlapped(curSize, blockSize, operationsCount, over_1, buffer, firstHandle);
+        WriteFileOverlapped(curSize, blockSize, operationsCount, over_2, buffer, secondHandle);
         curSize -= (long long)(blockSize*operationsCount);
     } while (curSize > 0);
 
-    SetFilePointer(secondHandle, fileSize, NULL, FILE_BEGIN);
+    SetFilePointerEx(secondHandle, fileSizeStruct, NULL, FILE_BEGIN);
     SetEndOfFile(secondHandle);
 }
